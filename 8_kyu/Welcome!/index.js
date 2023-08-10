da7a083e7a126a2c363f580419e5ce651adfa929
Yull Timoteo <yull.timoteo.z@uni.pe>
2023-08-10 12:42:04 -0500

    Adding 8kyu level problem: Welcome

---
+++ 8_kyu/Welcome!/index.js
@@ -0,0 +1,28 @@
+function greet(language) {
+  let languageAll = {
+    english: "Welcome",
+    czech: "Vitejte",
+    danish: "Velkomst",
+    dutch: "Welkom",
+    estonian: "Tere tulemast",
+    finnish: "Tervetuloa",
+    flemish: "Welgekomen",
+    french: "Bienvenue",
+    german: "Willkommen",
+    irish: "Failte",
+    italian: "Benvenuto",
+    latvian: "Gaidits",
+    lithuanian: "Laukiamas",
+    polish: "Witamy",
+    spanish: "Bienvenido",
+    swedish: "Valkommen",
+    welsh: "Croeso",
+  };
+
+  return languageAll[language] || languageAll["english"];
+}
+
+// test
+console.log(greet("english"));
+console.log(greet("dutch"));
+console.log(greet("IP_ADDRESS_INVALID"));
