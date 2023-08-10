9adef9daade4ed1a93144127accd0b67f87ac708
Yull Timoteo <yull.timoteo.z@uni.pe>
2023-08-10 08:10:13 -0500

    Adding 8kyu level problem: Is n divisible by x and y?.

---
+++ 8_kyu/Is n divisible_by_x_and y?/index.js
@@ -0,0 +1,14 @@
+function isDivisible(n, x, y) {
+  if (n % x == 0 && n % y == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// test
+
+console.log(isDivisible(3, 3, 4));
+console.log(isDivisible(12, 3, 4));
+console.log(isDivisible(8, 3, 4));
+console.log(isDivisible(48, 3, 4));
